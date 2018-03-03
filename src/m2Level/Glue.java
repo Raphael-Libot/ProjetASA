1f9d76719a2e26436413f2e7c2920645d48006d7
Raphael Libot <raphael.libot@gmail.com>
2018-03-03 17:13:22 +0100

    [GENERAL] cration des classes du modèle m2

---
+++ src/m2Level/Glue.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m2Level;
+
+/**
+ *
+ * @author rapha
+ */
+public class Glue {
+    
+    private InterfaceConnector From;
+    private InterfaceConnector To;
+    
+    
+    
+}
---
+++ src/m2Level/InterfaceComposant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m2Level;
+
+import java.util.List;
+
+/**
+ *
+ * @author rapha
+ */
+public class InterfaceComposant extends Interface {
+    
+    private List<Port> port;
+    private List<Service> services;
+    
+}
